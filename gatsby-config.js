58075c4dff4c0ca4ecfa862e59806c35ec9ce01d
josephdrazen <joe.malovic@gmail.com>
2021-11-14 12:53:57 -0500

    adds initial site skeleton

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `COMP2406 - Unofficial Student Docs`,
+    defaultTitle: `COMP2406 - Unofficial Student Docs`,
+    siteTitleShort: `COMP2406 - Docs - Unofficial`,
+    siteDescription: `COMP2406 Documentation Sourced By Students`,
+    siteUrl: `https://rocketdocs.netlify.app`,
+    siteAuthor: `@jpedroschmitz`,
+    siteImage: `/favicon.png`,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
+  },
+  flags: { PRESERVE_WEBPACK_CACHE: true },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        repositoryUrl: `https://github.com/jpedroschmitz/rocketdocs`,
+        baseDir: `examples/gatsby-theme-docs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocket Docs`,
+        short_name: `Rocket Docs`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `YOUR_ANALYTICS_ID`,
+    //   },
+    // },
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.app`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Path to the logo file on your project
+import logo from './logo.png';
+
+const Logo = () => (
+  <img src={logo} alt="COMP2406 Docs logo" style={{ width: 180 }} />
+);
+
+export default Logo;
